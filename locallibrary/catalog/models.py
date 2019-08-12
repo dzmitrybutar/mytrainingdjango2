a3bec7c8bdf36e6f7b47f5e1137a633a465ddd52
dzmitrybutar <verysweetleaf@gmail.com>
2019-08-12 14:59:49 +0300

    Migrate

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -48,7 +48,7 @@ class BookInstance(models.Model):
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_lenght=100)
+    last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
 
