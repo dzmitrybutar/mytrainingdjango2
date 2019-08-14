b15d16800c8b533ad1bd53bee1273e1bfb1b055d
dzmitrybutar <verysweetleaf@gmail.com>
2019-08-14 09:45:03 +0300

    Update register models

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,9 +1,42 @@
 from django.contrib import admin
-
 from .models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
+
 admin.site.register(Genre)
-admin.site.register(BookInstance)
 admin.site.register(Language)
+
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    list_display = ('id', 'book', 'status', 'due_back')
+    fieldsets = (
+        ('Book Instance', {
+            'fields': ('book', 'impint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
