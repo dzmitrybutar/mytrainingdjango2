e4b5c58ba96c92de5e718a40302b62ef509a4c0c
dzmitrybutar <verysweetleaf@gmail.com>
2019-08-14 15:09:50 +0300

    Authentication creation

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -2,19 +2,20 @@ from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 
+
 def index(request):
     num_books=Book.objects.all().count()
     num_instances=BookInstance.objects.all().count()
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()
-    num_visits=request.session.get('num_visits', 0)
+    num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits+1
-    context={'num_books':num_books,
-             'num_instances':num_instances,
-             'num_instances_available':num_instances_available,
-             'num_authors':num_authors,
-             'num_visits':num_visits
-             }
+    context = {'num_books': num_books,
+               'num_instances': num_instances,
+               'num_instances_available': num_instances_available,
+               'num_authors': num_authors,
+               'num_visits': num_visits
+              }
     return render(request, 'index.html', context=context)
 
 
